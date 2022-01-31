e9620ffa5dfb6ca721c839158a07f9d4d00f05d6
Miguel Nazario <mnazario35@gmail.com>
2022-01-31 13:22:50 -0500

    commit tree 13e01b9253bcaceb47e30b55c73b732d4a6558be

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    //returns the number of blocks given a value
+    return Math.abs(42 - pickup);
+  }
+
+  function distanceFromHqInFeet(pickup) {
+    const pickupDistance = distanceFromHqInBlocks(pickup);
+    return pickupDistance * 264;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    return Math.abs(destination - start) * 264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    const distanceFeet = distanceTravelledInFeet(start, destination);
+    if (distanceFeet > 2500) {
+        return 'cannot travel that far';
+    } else if (distanceFeet <= 400) {
+        return 0;
+    } else if (distanceFeet > 400 && distanceFeet <= 2000) {
+        return (distanceFeet - 400) * .02;
+    } else if (distanceFeet > 2000 && distanceFeet <= 2500) {
+        return 25;
+    }
+  }
